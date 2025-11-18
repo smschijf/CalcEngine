b4009e3b57a424d02412761e23a4c6c900464665
Sander Schijf <s.mschijf@casema.nl>
2025-11-18 16:52:04 +0100

    implemented command-line arguments

---
+++ src/com/pluralsight/calcengine/App.java
@@ -0,0 +1,92 @@
+package com.pluralsight.calcengine;
+
+public class App {
+    public static void main(String[] args) {
+        // double value1 = 100.0d;
+        // double value2 = 50.0d;
+        // double result = 0.0d;
+        // char opCode = 'd';
+
+        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        double[] results = new double[opCodes.length];
+
+        // if (opCode == 'a')
+        //     result = value1 + value2;
+        // else if (opCode == 's')
+        //     result = value1 - value2;
+        // else if (opCode == 'm')
+        //     result = value1 * value2;
+        // else if (opCode == 'd')
+        //     result = value1 / value2;
+        // else {
+        //     System.out.println("Invalid opCode: " + opCode);
+        //     result = 0.0d;
+        // }
+
+        if(args.length == 0) {
+            for(int i = 0; i < opCodes.length; i++) {
+                results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+            }
+
+        // switch(opCode) {
+        //     case 'a':
+        //         result = value1 + value2;
+        //         break;
+        //     case 's':
+        //         result = value1 - value2;
+        //         break;
+        //     case 'm':
+        //         result = value1 * value2;
+        //         break;
+        //     case 'd':
+        //         result = value2 != 0 ?value1 / value2 : 0.0d;
+        //         break;
+        //     default:
+        //         System.out.println("Invalid opCode: " + opCode);
+        //         result = 0.0d;
+        // }
+
+        for(double result : results)
+            System.out.println("result: " + result);
+        } else if(args.length == 3)
+            handleCommandLine(args);
+        else
+            System.out.println("Please provide an operation code and 2 numeric values");
+
+        for(int i = 1; i < 100; i *= 2)
+            System.out.println(i);
+    }
+
+    private static void handleCommandLine(String[] args) {
+        char opCode = args[0].charAt(0);
+        double leftVal = Double.parseDouble(args[1]);
+        double rightVal = Double.parseDouble(args[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+    }
+
+    static double execute(char opCode, double leftVal, double rightVal) {
+        double result;
+        switch (opCode) {
+                case 'a':
+                    result = leftVal + rightVal;
+                    break;
+                case 's':
+                    result = leftVal - rightVal;
+                    break;
+                case 'm':
+                    result = leftVal * rightVal;
+                    break;
+                case 'd':
+                    result = leftVal / rightVal;
+                    break;
+                default:
+                    System.out.println("Invalid opCode: " + opCode);
+                    result = 0.0d;
+                    break;
+        }
+        return result;
+    }
+}
